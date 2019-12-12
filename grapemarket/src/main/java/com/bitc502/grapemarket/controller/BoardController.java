2646a0e6f07666c01ea924b759feda397871cff4
sunbaL Kim <kimsunbal@naver.com>
2019-12-12 18:06:38 +0900

    write 추가

--- grapemarket/src/main/java/com/bitc502/grapemarket/controller/BoardController.java
+++ grapemarket/src/main/java/com/bitc502/grapemarket/controller/BoardController.java
@@ -1,19 +1,33 @@
 package com.bitc502.grapemarket.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bitc502.grapemarket.model.Board;
 import com.bitc502.grapemarket.model.Category;
+import com.bitc502.grapemarket.model.User;
+import com.bitc502.grapemarket.repository.BoardRepository;
 import com.bitc502.grapemarket.repository.CategoryRepository;
+import com.bitc502.grapemarket.repository.UserRepository;
+import com.bitc502.grapemarket.security.MyUserDetails;
 
 @Controller
 @RequestMapping("board")
 public class BoardController {
+	
+	@Autowired
+	private UserRepository uRepo;
+
+	@Autowired
+	private BoardRepository bRepo;
 
 	@Autowired
 	private CategoryRepository cRepo;
@@ -32,8 +46,24 @@ public class BoardController {
 
 	// 글쓰기 페이지
 	@GetMapping("/writeForm")
-	public String writeForm() {
+	public String writeForm(@AuthenticationPrincipal MyUserDetails userDetail, Model model) {
+		Optional<User> oUser = uRepo.findById(userDetail.getUser().getId());
+		User user = oUser.get();
+		model.addAttribute("user", user);
 		return "/board/write";
+	}
+	
+	@PostMapping("/writeProc")
+	public String write(Board board) {
+		try {
+			System.out.println(board);
+			bRepo.save(board);
+//			리스트 완성되면 바꿔야함
+			return "/board/category";
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return "redirect:/board/writeForm";
 	}
 
 	@GetMapping("/category")
