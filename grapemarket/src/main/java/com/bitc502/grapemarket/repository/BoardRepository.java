9f0d0ef74fb0a453eca407ff269be60d158289cb
Junhwon Kim <55569886+ssonsall@users.noreply.github.com>
2019-12-13 22:43:09 +0900

    BoardRepository countFindByCategory(int category) 추가. 빼먹어서..

--- grapemarket/src/main/java/com/bitc502/grapemarket/repository/BoardRepository.java
+++ grapemarket/src/main/java/com/bitc502/grapemarket/repository/BoardRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bitc502.grapemarket.model.Board;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
-
+	int countFindByCategory(int category);
 }
