7f705dea58892f3178be52ff6bd1d019b760b95d
aoperat <aoperat@naver.com>
2020-01-15 18:10:13 +0900

    판매완료기능
    채팅창에서 판매완료 버튼 입력시
    게시물로 이동하게 됨
    
    자신이 쓴 게시물에서는 구매 완료버튼을 입력한 유저 이름 리스트를 선택할 수 있는 리스트를 볼 수 있음.
    
    유저 이름 선택 후 거래완료 버튼 입력시 board 테이블에 buyer 칼럼에 유저 아이디가 입력되고, board 테이블 state 칼럼에는 상태값이 0 에서 1로 변경됨
    
    -> 리스트 출력시 state 가 0이 아닌 게시물은 화면에 표시하지 않도록 수정해야함.

--- grapemarket/src/main/java/com/bitc502/grapemarket/model/Board.java
+++ grapemarket/src/main/java/com/bitc502/grapemarket/model/Board.java
@@ -57,6 +57,11 @@ public class Board {
 	@JsonIgnoreProperties({ "comment","board","like" })
 	private User user;
 
+	@ManyToOne(cascade = CascadeType.MERGE)
+	@JoinColumn(name = "buyerId")
+	@JsonIgnoreProperties({ "comment","board","like" })
+	private User Buyer;
+	
 	// 상품 사진 시작
 	private String image1;
 	private String image2;
