d482971a3c61f76410a815b7d796ffbaebb77cb5
ssonsall <55569886+ssonsall@users.noreply.github.com>
2020-02-07 11:48:00 +0900

    종환씨 파일 20200207

--- grapemarket/src/main/java/com/bitc502/grapemarket/common/ReportType.java
+++
@@ -1,7 +0,0 @@
-package com.bitc502.grapemarket.common;
-
-public enum ReportType {
-	board,
-	comment,
-	message
-}
--- grapemarket/src/main/java/com/bitc502/grapemarket/payload/ReportFormData.java
+++
@@ -1,9 +0,0 @@
-package com.bitc502.grapemarket.payload;
-
-import lombok.Data;
-
-@Data
-public class ReportFormData {
-private Object object;
-private String type;
-}
